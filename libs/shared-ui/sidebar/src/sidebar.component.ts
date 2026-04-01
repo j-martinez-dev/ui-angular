6558a6b80171ef6f9e2a817a6ba542909e6a6059
Jaime Martinez <contact@j-martinez.dev>
2026-04-01 21:25:06 +0200

    feat(sidebar): Add avatar for titles when colapased

--- libs/shared-ui/sidebar/src/sidebar.component.ts
+++ libs/shared-ui/sidebar/src/sidebar.component.ts
@@ -2,18 +2,21 @@ import { Component, input, output, signal } from '@angular/core';
 import { UiIconComponent } from '@ui/shared-ui/icon';
 import { UiIconButtonComponent } from '@ui/shared-ui/icon-button';
 import { UiBadgeComponent } from '@ui/shared-ui/badge';
+import { UiAvatarComponent } from '@ui/shared-ui/avatar';
 import { UiTooltipDirective } from '@ui/shared-ui/tooltip';
+import { type AvatarColor } from '@ui/shared-ui/avatar';
 import { type SidebarItem, type SidebarGroup } from './sidebar.types';
 
 @Component({
   selector: 'ui-sidebar',
-  imports: [UiIconComponent, UiIconButtonComponent, UiBadgeComponent, UiTooltipDirective],
+  imports: [UiIconComponent, UiIconButtonComponent, UiBadgeComponent, UiAvatarComponent, UiTooltipDirective],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class UiSidebarComponent<T = string> {
   groups = input.required<SidebarGroup<T>[]>();
   activeValue = input<T>();
+  groupAvatarColor = input<AvatarColor>('accent-1');
 
   activeValueChange = output<T>();
 
